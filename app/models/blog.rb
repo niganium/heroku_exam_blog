aab75ac0a5c9cf04c13ac33d2108ca6783ab5a31
kotarou_nishio <sonnabakananz@gmail.com>
2018-06-01 01:21:41 +0000

    バリデーションを変更

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
-    validates :content,    length: { maximum: 75 }  
+    validates :content,    length: { maximum: 140 }  
 end
