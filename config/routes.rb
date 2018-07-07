503c7cbc9910342016d5502a9f8462a05aaf07ca
kotarou_nishio <sonnabakananz@gmail.com>
2018-07-07 02:38:30 +0000

    login-form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/', to: "home#top"
+  get '/about', to: "home#about"
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new,:create, :show]
 end
