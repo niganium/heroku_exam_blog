0e93bbccc2619e0e1c770bee20c72893d2184c74
kotarou_nishio <sonnabakananz@gmail.com>
2018-07-09 05:39:23 +0000

    mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :contacts
   get 'sessions/new'
 
   get '/', to: "home#top"
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new,:create, :show]
   resources :favorites, only: [:create, :destroy]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
