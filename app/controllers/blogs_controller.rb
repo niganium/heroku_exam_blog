c5927824e1ad26e0e9903d85cf0ca343545f9ff8
kotarou_nishio <sonnabakananz@gmail.com>
2018-07-09 11:54:13 +0000

    メイラー

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,6 +20,7 @@ class BlogsController < ApplicationController
     @blog.user_id = current_user.id
     
     if @blog.save
+      BlogMailer.blog_mail(@blog).deliver
       redirect_to blogs_path, notice: "ブログを作成しました！"
     else
       render 'new'
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -28,7 +28,6 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        ContactMailer.contact_mail(@contact).deliver
         format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
         format.json { render :show, status: :created, location: @contact }
       else
