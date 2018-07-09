3155e5d3887e2d536e5c1c2e2dd28dd4c8b716b2
kotarou_nishio <sonnabakananz@gmail.com>
2018-07-09 08:44:39 +0000

    heroku_mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
 def contact_mail(contact)
     @contact = contact
 
-    mail to: "@contact.user.email", subject: "お問い合わせの確認メール"
+    mail to: "sonnabakananz@gmail.com", subject: "お問い合わせの確認メール"
 end
 end
